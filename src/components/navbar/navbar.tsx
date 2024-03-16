650015aa2018b620fa1411c1b255a6248a7bfae9
MiroiuGabriel <miroiugabriel1231@gmail.com>
2024-03-16 16:10:32 +0200

    blog and snippets moved to medium

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -9,25 +9,15 @@ type NavbarProps = HTMLAttributes<HTMLElement>;
 
 export function Navbar({ className, ...props }: NavbarProps) {
 	return (
-		<nav
-			className={cn(
-				'flex justify-between items-center pt-8 pb-6 relative',
-				className
-			)}
-			{...props}
-		>
+		<nav className={cn('flex justify-between items-center pt-8 pb-6 relative', className)} {...props}>
 			<SkipToMain />
 			<div className="flex -ml-3 space-x-2 h-10">
-				<NavLink
-					href="/"
-					className="text-primary"
-					aria-label="brand logo"
-				>
+				<NavLink href="/" className="text-primary" aria-label="brand logo">
 					<Icon name="brand" />
 				</NavLink>
-				<NavLink href="/">Home</NavLink>
+				{/* <NavLink href="/">Home</NavLink>
 				<NavLink href="/snippets">Snippets</NavLink>
-				<NavLink href="/blog">Blog</NavLink>
+				<NavLink href="/blog">Blog</NavLink> */}
 			</div>
 			<ThemeButton />
 		</nav>
